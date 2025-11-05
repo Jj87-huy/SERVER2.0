c69182892bba889067003a9ccd65b9bf0a69f9bc
Jj87-huy <haoallain964@gmail.com>
2025-11-05 14:16:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // ===========================
 app.use(cors({
   origin: ["https://kbot-ai.name.vn", "http://localhost:3000"],
-  methods: methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.options("*", cors()); // ✅ Cho phép preflight request (quan trọng với POST).
