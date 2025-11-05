cfe2ccc33903cf7c7927ea17447f22d98fcfa5fe
Jj87-huy <haoallain964@gmail.com>
2025-11-05 14:58:19 +0700

    Update ai1_analyze.js

--- ai1_analyze.js
+++ ai1_analyze.js
@@ -4,10 +4,16 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI("AIzaSyAGZcayD9G0-j0CMyOI9Znwd2U19_rpvR4");
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
+const dup = {
+  "cooling fan":"cpu fan"
+};
+
 async function analyzeText(text) {
   try {
-    const prompt = `Phân tích ý chính của câu (VD ý chính: Định nghĩa Quạt CPU) "${text}".
+    const prompt = `Phân tích ý chính của câu (VD ý chính: Quạt CPU) "${text}".
      LƯU Ý:
+    - dịch ý chính vừa tìm sang tiếng anh.
+    - lấy "${dup}" nếu có từ giống, không thì tiếp tục.
     - Không viết gì thêm sau tên.`;
     const result = await model.generateContent(prompt);
     const keywords = (await result.response.text()).trim();
